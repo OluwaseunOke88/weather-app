5f2eeb876eeea08b65980f1c4dfe56c5adecefb3
OluwaseunOke88 <oluwaseunoke2015@gmail.com>
2025-03-25 20:50:12 -0400

    Add root route to handle GET /

--- server.js
+++ server.js
@@ -50,3 +50,8 @@ app.get("/forecast", async (req, res) => {
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Weather App Backend!');
+  });
+  
